6151cd8dba8fa7495ffe107151102a7578531c42
kholilmuflikhun <kholilmuflikhun@gmail.com>
2026-09-20 16:27:07 +0700

    fix: update import paths in next-env.d.ts and enhance Permissions-Policy in next.config.js

--- next.config.js
+++ next.config.js
@@ -27,7 +27,8 @@ const securityHeaders = [
   { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
   {
     key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+    value:
+      "camera=(), microphone=(), geolocation=(), interest-cohort=(), join-ad-interest-group=(), run-ad-auction=()",
   },
 ];
 
